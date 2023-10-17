f830886e72efef7dbcc01d7cbb616d07a6f2d336
Gustavo Henrique Ribeiro Meneses - 821135489 <821135489@ulife.com.br>
2023-10-16 21:46:02 -0300

    Implementação do model, controller e repository

---
+++ src/test/java/dio/myweb/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
